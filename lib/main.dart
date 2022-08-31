db8112dfa384e4f32c7eb9c7d93d121212bbb0e5
mayumi nina <Mayunina55@gmail.com>
2022-08-30 23:15:45 -0400

    modificando el calculator state

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
+import 'package:calculadora/bloc/calculator_bloc.dart';
 import 'package:calculadora/screens/calculator_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
- 
 void main() => runApp(MyApp());
- 
+
+class AppState extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(providers: [
+      BlocProvider<CalculatorBloc>(
+        create: (_) => CalculatorBloc(),
+        child: Container(),
+      )
+    ], child: MyApp());
+  }
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -11,9 +24,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: CalculatorScreen(),
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: Colors.black
-      ),
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: Colors.black),
     );
   }
 }
