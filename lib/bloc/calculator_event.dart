a9ad4b39adca29527ff330fe3a3d748825e83ffc
mayumi nina <Mayunina55@gmail.com>
2022-08-30 23:03:17 -0400

    modificando el calculator state

--- lib/bloc/calculator_event.dart
+++ lib/bloc/calculator_event.dart
@@ -1,4 +1,12 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'calculator_bloc.dart';
 
 @immutable
 abstract class CalculatorEvent {}
+
+class ResetAC extends CalculatorEvent {}
+
+class AddNumber extends CalculatorEvent {
+  final String number;
+  AddNumber(this.number);
+}
